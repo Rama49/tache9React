e58b1a5fa97cb5f2e66cdbdded83f54f3d3a5b75
Rama Fall <rama.fall49@gmail.com>
2023-05-15 17:17:02 +0000

    deuxieme commit

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -4,18 +4,18 @@ import React, { useState } from "react";
 const Welcome = (props) => {
   const [count, setCount] = useState(0);
   return (
-    <div className="h-75 w-25 align-items-center text-center mt-5 shadow">
-      <h1>Bienvenu: {props.name}</h1>
+    <div className="h-75 w-50 align-items-center text-center mt-5 shadow">
+      <h1 className="mt-3">Bienvenu: {props.name}</h1>
       <p>Mon compteur : {count}</p>
       <div className="align-items-center">
         <button
-          className="bg-white me-3 text-decoration-none"
+          className="bg-white me-3 text-decoration-none mb-3"
           onClick={() => setCount(count + 1)}
         >
           Ajouter
         </button>
         <button
-          className="bg-white text-decoration-none"
+          className="bg-white text-decoration-none mb-3"
           onClick={() => setCount(count - 1).bind(this)}
         >
           Diminuer
